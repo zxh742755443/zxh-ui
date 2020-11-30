af3de54a81f98203d59f711d09adbfb6685eb4f3
zhangxiaohu <zhangxiaohu1@tuhu.cn>
2020-11-30 18:50:43 +0800

    feat:图片压缩

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="demo-image__lazy">
+  <div class="demo-compress">
     <input type="file" @change="upload"/>
+    <table class="demo-table" v-show="nowSize">
+      <thead>
+        <tr>
+          <th>文件名</th>
+          <th>压缩前大小</th>
+          <th>压缩后大小</th>
+          <th>压缩比</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>u=504110440,1027090340&fm=26&gp=0.jpg</td>
+          <td>{{oldSize}} KB</td>
+          <td>{{nowSize}} KB</td>
+          <td>{{ ((oldSize-nowSize)/oldSize*100).toFixed(1)}}%</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -10,13 +28,20 @@ import compress from './utils/compress';
 export default {
   name: 'App',
   data() {
-    return {};
+    return {
+      oldSize: '',
+      fileName: '',
+      nowSize: ''
+    };
   },
   methods: {
-    upload(e) {
-      var file = e.target.files[0];
-      compress(file, {});
-      console.log('----------', file);
+    async upload(e) {
+      let file = e.target.files[0];
+      this.oldSize = (file.size / 1024).toFixed(1);
+      this.fileName = file.name;
+      let res = await compress(file);
+      window.open(window.URL.createObjectURL(res));
+      this.nowSize = (res.size / 1024).toFixed(1);
     }
   }
 };
@@ -24,4 +49,50 @@ export default {
 
 <style lang="scss">
 
+.demo-compress{
+  margin-top: 30px;
+
+  td,th {
+    padding: 0;
+  }
+
+  .demo-table {
+    border-collapse: collapse;
+    border-spacing: 0;
+    empty-cells: show;
+    border: 1px solid #cbcbcb;
+    margin-top: 20px;
+  }
+
+  .demo-table caption {
+    color: #000;
+    font: italic 85%/1 arial,sans-serif;
+    padding: 1em 0;
+    text-align: center;
+  }
+
+  .demo-table td,.demo-table th {
+    border-left: 1px solid #cbcbcb;
+    border-width: 0 0 0 1px;
+    font-size: inherit;
+    margin: 0;
+    overflow: visible;
+    padding: .5em 1em;
+  }
+
+  .demo-table thead {
+    background-color: #e0e0e0;
+    color: #000;
+    text-align: left;
+    vertical-align: bottom;
+  }
+
+  .demo-table td {
+    background-color: transparent;
+  }
+
+  .demo-table-odd td {
+    background-color: #f2f2f2;
+  }
+}
 </style>
--- src/utils/compress.js
+++ src/utils/compress.js
@@ -1,189 +1,111 @@
-'use strict';
+import EXIF from 'exif-js';
 
-exports.__esModule = true;
-
-// private int getOrientation(byte[] jpeg) {
-//   if (jpeg == null) {
-//     return 0;
-//   }
-//
-//   int offset = 0;
-//   int length = 0;
-//
-//   // ISO/IEC 10918-1:1993(E)
-//   while (offset + 3 < jpeg.length && (jpeg[offset++] & 0xFF) == 0xFF) {
-//     int marker = jpeg[offset] & 0xFF;
-//
-//     // Check if the marker is a padding.
-//     if (marker == 0xFF) {
-//       continue;
-//     }
-//     offset++;
-//
-//     // Check if the marker is SOI or TEM.
-//     if (marker == 0xD8 || marker == 0x01) {
-//       continue;
-//     }
-//     // Check if the marker is EOI or SOS.
-//     if (marker == 0xD9 || marker == 0xDA) {
-//       break;
-//     }
-//
-//     // Get the length and check if it is reasonable.
-//     length = pack(jpeg, offset, 2, false);
-//     if (length < 2 || offset + length > jpeg.length) {
-//       Log.e(TAG, "Invalid length");
-//       return 0;
-//     }
-//
-//     // Break if the marker is EXIF in APP1.
-//     if (marker == 0xE1 && length >= 8
-//       && pack(jpeg, offset + 2, 4, false) == 0x45786966
-//       && pack(jpeg, offset + 6, 2, false) == 0) {
-//       offset += 8;
-//       length -= 8;
-//       break;
-//     }
-//
-//     // Skip other markers.
-//     offset += length;
-//     length = 0;
-//   }
-//
-//   // JEITA CP-3451 Exif Version 2.2
-//   if (length > 8) {
-//     // Identify the byte order.
-//     int tag = pack(jpeg, offset, 4, false);
-//     if (tag != 0x49492A00 && tag != 0x4D4D002A) {
-//       Log.e(TAG, "Invalid byte order");
-//       return 0;
-//     }
-//     boolean littleEndian = (tag == 0x49492A00);
-//
-//     // Get the offset and check if it is reasonable.
-//     int count = pack(jpeg, offset + 4, 4, littleEndian) + 2;
-//     if (count < 10 || count > length) {
-//       Log.e(TAG, "Invalid offset");
-//       return 0;
-//     }
-//     offset += count;
-//     length -= count;
-//
-//     // Get the count and go through all the elements.
-//     count = pack(jpeg, offset - 2, 2, littleEndian);
-//     while (count-- > 0 && length >= 12) {
-//       // Get the tag and check if it is orientation.
-//       tag = pack(jpeg, offset, 2, littleEndian);
-//       if (tag == 0x0112) {
-//         int orientation = pack(jpeg, offset + 8, 2, littleEndian);
-//         switch (orientation) {
-//           case 1:
-//             return 0;
-//           case 3:
-//             return 180;
-//           case 6:
-//             return 90;
-//           case 8:
-//             return 270;
-//         }
-//         Log.e(TAG, "Unsupported orientation");
-//         return 0;
-//       }
-//       offset += 12;
-//       length -= 12;
-//     }
-//   }
-//
-//   Log.e(TAG, "Orientation not found");
-//   return 0;
-// }
-const _fileFun = {};
-const _config = Object.create(_fileFun);
-const _base = 1280;
+let _config = null;
 
 /**
  * 通过 url 加载所需要的图片对象
  * @param url
- * @param fn
+ * @param cb
  */
-_fileFun.urltoImage = (url, fn) =>{
+export function urltoImage(url, cb) {
   let img = new Image();
   img.src = url;
+  img.crossOrigin = '';
   img.onload = function() {
-    fn(img);
+    cb && cb(img);
   };
-};
+}
 
 /**
  * 将 Image 对象转变为 Canvas 类型对象
  * @param image
  * @returns {HTMLCanvasElement}
  */
-_fileFun.imagetoCanvas = (image) =>{
-  let cvs = document.createElement('canvas');
-  let ctx = cvs.getContext('2d');
-  cvs.width = image.width;
-  cvs.height = image.height;
-  ctx.drawImage(image, 0, 0, cvs.width, cvs.height);
-  return cvs ;
-};
+export function imagetoCanvas(image) {
+  let canvas = document.createElement('canvas');
+  let context = canvas.getContext('2d');
+  let w = image.width;
+  let h = image.height;
+  switch (_config.orientation) {
+    case 6: // 需要顺时针90度旋转
+      canvas.width = h;
+      canvas.height = w;
+      context.rotate((90 * Math.PI) / 180);
+      context.drawImage(image, 0, -h, w, h);
+      break;
+    case 8: // 需要逆时针90度旋转
+      canvas.width = h;
+      canvas.height = w;
+      context.rotate((-90 * Math.PI) / 180);
+      context.drawImage(image, -w, 0, w, h);
+      break;
+    case 3: // 需要180度旋转
+      context.rotate((180 * Math.PI) / 180);
+      context.drawImage(image, -w, -h, w, h);
+      break;
+    default:
+      canvas.width = w;
+      canvas.height = h;
+      context.drawImage(image, 0, 0, w, h);
+  }
+  return canvas ;
+}
 
 /**
- * 将 Canvas 对象压缩转变为 Blob 类型对象
+ * 将 canvas 对象压缩转变为 Blob 类型对象
  * @param canvas
  * @param quality
- * @param fn
+ * @param cb
  */
-_fileFun.canvasResizetoFile = (canvas, quality, fn) =>{
+export function canvasResizetoFile(canvas, quality, cb) {
   canvas.toBlob(function(blob) {
-    fn(blob);
-  }, 'image/jpeg', quality);
-};
+    blob.name = _config.name;
+    cb && cb(blob);
+  }, _config.type, quality);
+}
 
 /**
- * 将 Canvas 对象压缩转变为 dataURL 字符串
+ * 将 canvas 对象压缩转变为 dataURL 字符串
  * @param canvas
  * @param quality
  * @returns {string}
  */
-_fileFun.canvasResizetoDataURL = (canvas, quality) =>{
-  return canvas.toDataURL('image/jpeg', quality);
-};
+export function canvasResizetoDataURL(canvas, quality) {
+  return canvas.toDataURL(_config.type, quality);
+}
 
 /**
  * 将 File （ Blob ）类型文件转变为 dataURL 字符串
  * @param file
- * @param fn
+ * @param cb
  */
-_fileFun.filetoDataURL = (file) =>{
+export function filetoDataURL(file, cb) {
   let reader = new FileReader();
   reader.onloadend = function(e) {
-    // fn(e.target.result);
-    _fileFun.dataURLtoImage(e.target.result);
+    cb && cb(e.target.result);
   };
   reader.readAsDataURL(file);
-};
+}
 
 /**
  * 将 dataURL 字符串转变为 Image 类型文件
  * @param dataurl
- * @param fn
+ * @param cb
  */
-_fileFun.dataURLtoImage = (dataurl) =>{
+export function dataURLtoImage(dataurl, cb) {
   let img = new Image();
   img.onload = function() {
-    // fn(img);
-    computeImgae(img);
+    cb && cb(img);
   };
   img.src = dataurl;
-};
+}
 
 /**
  * 将 dataURL 字符串转变为 Blob 类型对象
  * @param dataurl
  * @returns {Blob}
  */
-_fileFun.dataURLtoFile = (dataurl) =>{
+export function dataURLtoFile(dataurl) {
   let arr = dataurl.split(',');
   let mime = arr[0].match(/:(.*?);/)[1];
   let bstr = atob(arr[1]);
@@ -194,46 +116,78 @@ _fileFun.dataURLtoFile = (dataurl) =>{
     u8arr[n] = bstr.charCodeAt(n);
   }
   return new Blob([u8arr], {type: mime});
-};
+}
 
+/**
+ * 设置图片宽高
+ * @param image
+ */
 function computeImgae(image) {
-  // const width = image.o
-  // console.log('---------', image.naturalWidth, image.naturalHeight);
   let width = image.naturalWidth;
   let height = image.naturalHeight;
-  if (width > _base || height > _base) {
+  if (width > _config.base && height > _config.base) {
     if (width > height) {
       const scale = height / width;
-      width = _base;
+      width = _config.base;
       height = width * scale;
     } else {
       const scale = width / height;
-      height = _base;
+      height = _config.base;
       width = height * scale;
     }
-  } else if (width > _base || height < _base) {
+  } else if (width > _config.base && height < _config.base) {
     const scale = height / width;
-    width = _base;
+    width = _config.base;
     height = width * scale;
-  } else if (width < _base || height > _base) {
+  } else if (width < _config.base && height > _config.base) {
     const scale = width / height;
-    height = _base;
+    height = _config.base;
     width = height * scale;
   }
   image.width = width;
   image.height = height;
+  engine(imagetoCanvas(image), 1);
 }
 
-function compress(file, options) {
-  _config.options = options;
+/**
+ * 压缩图片质量
+ * @param canvas
+ * @param quality
+ */
+function engine(canvas, quality) {
+  canvasResizetoFile(canvas, quality, (blob)=>{
+    if (blob.size > _config.size) {
+      if (quality - _config.qualityStep <= 0.5) {
+        _config.resolve(blob);
+        return;
+      }
+      engine(canvas, quality - _config.qualityStep);
+    } else {
+      _config.resolve(blob);
+    }
+  });
+}
 
-  console.log('11111----------', typeof file, options);
+export default function compress(file, options) {
+  if (!file || !/image\/(png|jpe?g)/.test(file.type)) {
+    throw new Error('please upload image[png,jpg,jpeg] file');
+  }
+  _config = Object.create(options || null);
+  _config.size = file.size;
+  _config.type = file.type.replace(/png$/, 'jpeg');
+  _config.name = file.name.replace(/\.png$/, '.jpeg');
+  _config.qualityStep = 0.1;
+  _config.base = 1080;
+
   // eslint-disable-next-line no-undef
   return new Promise((resolve, reject)=>{
     _config.resolve = resolve;
     _config.reject = reject;
-    _fileFun.filetoDataURL(file);
+    EXIF.getData(file, () => {
+      _config.orientation = EXIF.getTag(file, 'Orientation');
+      filetoDataURL(file, (data)=>{
+        dataURLtoImage(data, computeImgae);
+      });
+    });
   });
 }
-
-exports.default = compress;
